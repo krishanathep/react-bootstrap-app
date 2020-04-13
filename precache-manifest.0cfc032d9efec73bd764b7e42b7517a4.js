5e862a7b63166d618d1ac8e5c9ae07059813afcf
krishanathep <krishanathep@gmail.com>
2020-04-13 16:06:33 +0700

    Updates

---
+++ precache-manifest.0cfc032d9efec73bd764b7e42b7517a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3023fe056f27010c487c30af31bfea",
+    "url": "/react-bootstrap-app/index.html"
+  },
+  {
+    "revision": "151e1689ffbbf933ae14",
+    "url": "/react-bootstrap-app/static/css/main.9522d84b.chunk.css"
+  },
+  {
+    "revision": "a381e3c74a2ec8a2ccb3",
+    "url": "/react-bootstrap-app/static/js/2.f4e6175f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-bootstrap-app/static/js/2.f4e6175f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151e1689ffbbf933ae14",
+    "url": "/react-bootstrap-app/static/js/main.ef8d8888.chunk.js"
+  },
+  {
+    "revision": "0a940e9b991ca161c9f8",
+    "url": "/react-bootstrap-app/static/js/runtime-main.4a89df79.js"
+  }
+]);
